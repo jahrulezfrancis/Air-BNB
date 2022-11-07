f6743372c9851ac3c3f3277bee5cc786253ae35b
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-11-07 10:22:44 +0100

    Initial commit

---
+++ src/Components/HomePage.jsx
@@ -0,0 +1,9 @@
+import Navigation from "./Navigation";
+
+export default function HomePage() {
+    return (
+        <div className="homePage">
+            <Navigation />
+        </div>
+    )
+}
---
+++ src/Components/Navigation.jsx
@@ -0,0 +1,10 @@
+import HeaderLogo from "../Images/airbnb-logo.png";
+
+
+export default function Navigation() {
+    return (
+        <div className="navigation">
+            <img src={HeaderLogo} alt='header logo' />
+        </div>
+    )
+}
