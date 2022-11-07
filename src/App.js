d5e67c1abaa56f9f22bc2c3b06f3cffff480f6ab
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-11-07 18:50:21 +0100

    added header section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import HomePage from './Components/HomePage';
+import TopSection from './Components/TopSection';
 
 function App() {
   return (
     <div className="App">
       <HomePage />
+      <TopSection />
     </div>
   );
 }
